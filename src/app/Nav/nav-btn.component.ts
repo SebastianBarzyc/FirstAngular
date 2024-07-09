697e3fe7cf6c82c3beb92b47555fd4b9e7fa49f1
SebastianBarzyc <121134777+SebastianBarzyc@users.noreply.github.com>
2024-07-09 20:02:43 +0200

    Top NavBar

--- src/app/Nav/nav-btn.component.ts
+++ src/app/Nav/nav-btn.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
     selector: 'navbtn',
     standalone: true,
-    imports: [MatBadgeModule],
     template: `
-    <button  mat-icon-button class="btn fw-bold"><ng-content></ng-content></button>
+    <button class="btn fw-bold"><ng-content></ng-content></button>
     `
 })
 
-export class NavBtnComponent {
-    
-};
+export class NavBtnComponent {};
