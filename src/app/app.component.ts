08cdda0ccdb75ccb3d837c4c1132d89b5d4a2f78
SebastianBarzyc <121134777+SebastianBarzyc@users.noreply.github.com>
2024-07-26 19:31:57 +0200

    clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './Nav/nav.component';
-import { NavBtnComponent } from './Nav/nav-btn.component';
-import { DashoardComponent } from './Dashoard/dashoard.component';
-import { CalendarComponent } from './Calendar/calendar.component';
-import { ExercisesComponent } from './Exercises/exercises.component';
-import { ProfileComponent } from './Profile/profile.component';
-import { ProgressComponent } from './Progress/progress.component';
-import { WorkoutsComponent } from './Workouts/workouts.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, NavComponent, NavBtnComponent, DashoardComponent, CalendarComponent, ExercisesComponent, ProfileComponent, ProgressComponent, ReactiveFormsModule, WorkoutsComponent],
+    RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
